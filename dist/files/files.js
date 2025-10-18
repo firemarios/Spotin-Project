c0ec2d61a9481454e5133e7c50f1ef6cbf453a73
firemarios <apostolos.katsatouris@gmail.com>
2025-10-19 00:13:42 +0300

    202510190013

--- dist/files/files.js
+++ dist/files/files.js
@@ -15,36 +15,24 @@ function NavigatingTo() {
         actionBar.innerHTML = localUtils.getActionBarHTML(false);
     }
 }
-for (let i = 0; i < 50; i++) {
-    const filesDiv = document.getElementById("file-viewer");
-    if (filesDiv) {
-        filesDiv.innerHTML += `
-        <div id="file">
-            <div id="content-box">
-                <div id="file-type"></div>
-            </div>
-            <p id="file-name">${i + ".pdf"}</p>
-        </div>`;
-    }
+const files = document.querySelectorAll('#file-type');
+const file_contentBox = document.querySelectorAll('#content-box');
+const file_names = document.querySelectorAll('#file-name');
+if (files && file_names && file_contentBox) {
+    files.forEach((element, index) => {
+        const el = element;
+        const name = file_names[index];
+        const box = file_contentBox[index];
+        if (name)
+            el.textContent = name.textContent;
+        const iconClass = localUtils.getFileIcon(el.innerHTML);
+        el.innerHTML = `<i class="${iconClass}"></i>`;
+        el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
+        if (box)
+            box.onclick = () => {
+                if (name)
+                    document.location = "./view/#/" + name.textContent;
+            };
+    });
 }
-// const files = document.querySelectorAll<HTMLElement>('#file-type');
-// const file_contentBox = document.querySelectorAll<HTMLElement>('#content-box');
-// const file_names = document.querySelectorAll('#file-name');
-// if (files && file_names && file_contentBox) {
-//     files.forEach((element, index) => {
-//         const el = element as HTMLElement;
-//         const name = file_names[index];
-//         const box = file_contentBox[index];
-//         if (name)
-//         el.textContent = name.textContent;
-//         const iconClass = localUtils.getFileIcon(el.innerHTML);
-//         el.innerHTML = `<i class="${iconClass}"></i>`;
-//         el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
-//         if (box)
-//         box.onclick = () => {
-//             if (name)
-//             document.location = "./view/#/" + name.textContent;
-//         };
-//     });
-// }
 //# sourceMappingURL=files.js.map
--- src/files/files.ts
+++ src/files/files.ts
@@ -18,42 +18,29 @@ function NavigatingTo() {
     }
 }
 
-for (let i = 0; i < 50; i++) {
-    const filesDiv = document.getElementById("file-viewer")
-    if (filesDiv) {
-        filesDiv.innerHTML += `
-        <div id="file">
-            <div id="content-box">
-                <div id="file-type"></div>
-            </div>
-            <p id="file-name">${i + ".pdf"}</p>
-        </div>`
-    }
+const files = document.querySelectorAll<HTMLElement>('#file-type');
+const file_contentBox = document.querySelectorAll<HTMLElement>('#content-box');
+const file_names = document.querySelectorAll('#file-name');
+
+if (files && file_names && file_contentBox) {
+    files.forEach((element, index) => {
+        const el = element as HTMLElement;
+        const name = file_names[index];
+        const box = file_contentBox[index];
+
+        if (name)
+
+        el.textContent = name.textContent;
+
+        const iconClass = localUtils.getFileIcon(el.innerHTML);
+        el.innerHTML = `<i class="${iconClass}"></i>`;
+        el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
+
+        if (box)
+
+        box.onclick = () => {
+            if (name)
+            document.location = "./view/#/" + name.textContent;
+        };
+    });
 }
-
-// const files = document.querySelectorAll<HTMLElement>('#file-type');
-// const file_contentBox = document.querySelectorAll<HTMLElement>('#content-box');
-// const file_names = document.querySelectorAll('#file-name');
-
-// if (files && file_names && file_contentBox) {
-//     files.forEach((element, index) => {
-//         const el = element as HTMLElement;
-//         const name = file_names[index];
-//         const box = file_contentBox[index];
-
-//         if (name)
-
-//         el.textContent = name.textContent;
-
-//         const iconClass = localUtils.getFileIcon(el.innerHTML);
-//         el.innerHTML = `<i class="${iconClass}"></i>`;
-//         el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
-
-//         if (box)
-
-//         box.onclick = () => {
-//             if (name)
-//             document.location = "./view/#/" + name.textContent;
-//         };
-//     });
-// }
