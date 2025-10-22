d2cc369e21c90686770f204ce374220f19e5abea
firemarios <apostolos.katsatouris@gmail.com>
2025-10-23 00:00:44 +0300

    202510230000

--- dist/home/home.js
+++ dist/home/home.js
@@ -1,6 +1,16 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { localUtils } from "../code/localUtils.js";
 localUtils.verifyRenewToken(false);
 NavigatingTo();
+getlatestFiles();
 const username_text = document.getElementById("username-text");
 if (username_text)
     username_text.textContent = localUtils.getCookie("name");
@@ -19,24 +29,41 @@ function NavigatingTo() {
         actionBar.innerHTML = localUtils.getActionBarHTML(false);
     }
 }
-const files = document.querySelectorAll('#file-type');
-const file_contentBox = document.querySelectorAll('#content-box');
-const file_names = document.querySelectorAll('#file-name');
-if (files && file_names && file_contentBox) {
-    files.forEach((element, index) => {
-        const el = element;
-        const name = file_names[index];
-        const box = file_contentBox[index];
-        if (name)
-            el.textContent = name.textContent;
-        const iconClass = localUtils.getFileIcon(el.innerHTML);
-        el.innerHTML = `<i class="${iconClass}"></i>`;
-        el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
-        if (box)
-            box.onclick = () => {
-                if (name)
-                    document.location = "../files/view/#/" + name.textContent;
-            };
+function getlatestFiles() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const recent_content = document.querySelector('.recent-content');
+        const response = yield localUtils.GET("files/latest", { "Authorization": "Bearer " + localUtils.getCookie("access_token") });
+        response.forEach((e) => {
+            if (recent_content)
+                recent_content.innerHTML += `<div id="content-box">
+                                        <div id="file-type" directory="${e.directory}"></div>
+                                        <p id="file-name">${e.name}</p>
+                                    </div>`;
+        });
+        setIcons();
     });
+}
+function setIcons() {
+    const files = document.querySelectorAll('#file-type');
+    const file_contentBox = document.querySelectorAll('#content-box');
+    const file_names = document.querySelectorAll('#file-name');
+    if (files && file_names && file_contentBox) {
+        files.forEach((element, index) => {
+            const el = element;
+            const name = file_names[index];
+            const box = file_contentBox[index];
+            const directory = el.getAttribute('directory') || '';
+            if (name)
+                el.textContent = name.textContent;
+            const iconClass = localUtils.getFileIcon(el.innerHTML);
+            el.innerHTML = `<i class="${iconClass}"></i>`;
+            el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
+            if (box)
+                box.onclick = () => {
+                    if (name)
+                        document.location = "../files/view/#/" + directory + "/" + name.textContent;
+                };
+        });
+    }
 }
 //# sourceMappingURL=home.js.map
--- src/home/home.ts
+++ src/home/home.ts
@@ -3,6 +3,7 @@ import { localUtils } from "../code/localUtils.js";
 localUtils.verifyRenewToken(false)
 
 NavigatingTo();
+getlatestFiles();
 
 const username_text = document.getElementById("username-text");
 
@@ -25,29 +26,45 @@ function NavigatingTo() {
     }
 }
 
-const files = document.querySelectorAll<HTMLElement>('#file-type');
-const file_contentBox = document.querySelectorAll<HTMLElement>('#content-box');
-const file_names = document.querySelectorAll('#file-name');
-
-if (files && file_names && file_contentBox) {
-    files.forEach((element, index) => {
-        const el = element as HTMLElement;
-        const name = file_names[index];
-        const box = file_contentBox[index];
-
-        if (name)
-
-        el.textContent = name.textContent;
-
-        const iconClass = localUtils.getFileIcon(el.innerHTML);
-        el.innerHTML = `<i class="${iconClass}"></i>`;
-        el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
-
-        if (box)
-
-        box.onclick = () => {
-            if (name)
-            document.location = "../files/view/#/" + name.textContent;
-        };
+async function getlatestFiles() {
+    const recent_content = document.querySelector('.recent-content');
+    const response: any = await localUtils.GET("files/latest", {"Authorization": "Bearer " + localUtils.getCookie("access_token")})
+    response.forEach((e: any) => {
+        if (recent_content)
+        recent_content.innerHTML += `<div id="content-box">
+                                        <div id="file-type" directory="${e.directory}"></div>
+                                        <p id="file-name">${e.name}</p>
+                                    </div>`
     });
+    setIcons();
+}
+
+function setIcons() {
+    const files = document.querySelectorAll<HTMLElement>('#file-type');
+    const file_contentBox = document.querySelectorAll<HTMLElement>('#content-box');
+    const file_names = document.querySelectorAll('#file-name');
+
+    if (files && file_names && file_contentBox) {
+        files.forEach((element, index) => {
+            const el = element as HTMLElement;
+            const name = file_names[index];
+            const box = file_contentBox[index];
+            const directory = el.getAttribute('directory') || '';
+
+            if (name)
+
+            el.textContent = name.textContent;
+
+            const iconClass = localUtils.getFileIcon(el.innerHTML);
+            el.innerHTML = `<i class="${iconClass}"></i>`;
+            el.style.setProperty('color', localUtils.getFileIconColor(iconClass));
+
+            if (box)
+
+            box.onclick = () => {
+                if (name)
+                document.location = "../files/view/#/" + directory + "/" + name.textContent;
+            };
+        });
+    }
 }
