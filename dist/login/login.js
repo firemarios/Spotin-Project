5a63c7503b5e7ed87a1ff4a8ede83bb73508b788
firemarios <apostolos.katsatouris@gmail.com>
2025-10-22 19:46:12 +0300

    202510221946

--- dist/login/login.js
+++ dist/login/login.js
@@ -7,7 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { localUtils } from "../code/localUtils";
+import { localUtils } from "../code/localUtils.js";
 function login() {
     return __awaiter(this, void 0, void 0, function* () {
         var _a, _b;
